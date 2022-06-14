8a9c2143120b16b56eb4bbc5562ceffd58b7473a
ivan91simeonov <vanches91@abv.bg>
2022-06-14 18:58:10 +0300

    Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,9 +11,30 @@ function Header() {
         <div className='nav-menu'>
             <Link className='link' to="/home">
               <img src="/images/home-icon.svg" alt="" />
-              <span>Home</span>
+              <span>HOME</span>
+            </Link>
+            <Link className='link' to="/search">
+              <img src="/images/search-icon.svg" alt="" />
+              <span>SEARCH</span>
+            </Link>
+            <Link className='link' to="/watchlist">
+              <img src="/images/watchlist-icon.svg" alt="" />
+              <span>WATCHLIST</span>
+            </Link>
+            <Link className='link' to="/original">
+              <img src="/images/original-icon.svg" alt="" />
+              <span>ORIGINAL</span>
+            </Link>
+            <Link className='link' to="/movies">
+              <img src="/images/movie-icon.svg" alt="" />
+              <span>MOVIES</span>
+            </Link>
+            <Link className='link' to="/series">
+              <img src="/images/series-icon.svg" alt="" />
+              <span>SERIES</span>
             </Link>
         </div>
+        <img src="/images/viewers-disney.png" alt="" className='user-image' />
       </nav>
     </header>
   )
