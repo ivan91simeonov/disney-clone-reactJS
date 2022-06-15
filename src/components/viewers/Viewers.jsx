031ac54dee1c4ca9a7e015df33809c93bfaa6bd0
ivan91simeonov <vanches91@abv.bg>
2022-06-15 22:30:21 +0300

    Viewers

--- src/components/viewers/Viewers.jsx
+++ src/components/viewers/Viewers.jsx
@@ -5,7 +5,22 @@ function Viewers () {
 
     return(
         <Container>
-            Viewers
+            <Wrap>
+                <img src="/images/viewers-disney.png" alt="" />
+            </Wrap>
+            <Wrap>
+                <img src="/images/viewers-pixar.png" alt="" />
+            </Wrap>
+            <Wrap>
+                <img src="/images/viewers-marvel.png" alt="" />
+            </Wrap>
+            <Wrap>
+                <img src="/images/viewers-starwars.png" alt="" />
+            </Wrap>
+            <Wrap>
+                <img src="/images/viewers-national.png" alt="" />
+            </Wrap>
+    
         </Container>
     )
 }
@@ -13,5 +28,29 @@ function Viewers () {
 export default Viewers
 
 const Container = styled.div`
+    margin-top: 3rem;
+    display: grid;
+    padding: 3rem 0 2.6rem;
+    grid-template-columns: repeat(5 , minmax(0 , 1fr));
+    grid-gap: 2.5rem;
+`
 
+const Wrap = styled.div`
+    border-radius: 1rem;
+    cursor: pointer;
+    border: 3px solid rgba(249 , 249, 249, 0.1);
+    box-shadow: rgb(0, 0, 0 / 69%) 0px 26px 30px -10px,
+    rgb(0, 0, 0 / 73%) 0px 16px 10px -10px;
+    transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+
+    img{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    &:hover{
+        transform: scale(1.05);
+        border-color: rgba(249 , 249, 249, 0.8);
+    }
 `
