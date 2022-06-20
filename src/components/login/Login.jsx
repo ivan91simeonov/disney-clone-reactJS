c2ec830da2cc6b4b0c66b754cc4d6cd2a316d988
ivan91simeonov <vanches91@abv.bg>
2022-06-20 22:10:01 +0300

    Login page

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,13 +1,20 @@
-import './Login.css'
+import './Login.css';
 
 function Login() {
   return (
-    <div className='login-container'>
-        <div className="cta-box">
-           <img src="/images/cta-logo-one.svg" alt="" />
-        </div>
+    <div className="login-container">
+      <div className="cta-box">
+        <img src="/images/cta-logo-one.svg" alt="" />
+        <a className="sign-up">GET ALL THERE</a>
+        <p className="description">
+          Get Premier Access to Raya and the Last Dragon for app with a Disney+
+          subscription. As of 03/26/21 , the price of The Disney Bundle will
+          increase by $1.
+        </p>
+        <img src="/images/cta-logo-two.png" alt="" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
